a1933e1ce597975b30f4cc73853364f046427db9
Petor Cheprasov <petor.tcheprasov@yandex.ru>
2025-10-14 21:19:12 +0700

    Ларек версия 1.1

--- src/components/Models/Buyer.ts
+++ src/components/Models/Buyer.ts
@@ -1,44 +1,113 @@
-import { IBuyer } from "../../../types";
+import { IBuyer } from "../../../types"; 
 
-export class Buyer {
-  protected buyer: IBuyer = {
-    'payment': '',
-    'address': '',
-    'phone': '',
-    'email': ''
-  };
+export class Buyer { 
+  protected buyer: IBuyer = { 
+    'payment': '', 
+    'address': '', 
+    'phone': '', 
+    'email': '' 
+  }; 
 
-  constructor() {
+  constructor() { 
+  } 
+
+  // Валидация отдельных полей
+  protected validateField(field: keyof IBuyer, value: string): boolean {
+    switch (field) {
+      case 'email':
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+        return emailRegex.test(value);
+      
+      case 'phone':
+        const phoneRegex = /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/;
+        return phoneRegex.test(value);
+      
+      case 'payment':
+        return value === 'card' || value === 'cash';
+      
+      case 'address':
+        return value.trim().length > 0;
+      
+      default:
+        return false;
+    }
   }
 
-  protected checkData(buyer: IBuyer): boolean {
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-    const phoneRegex = /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/;
+  // Универсальный метод для обновления любого поля
+  public updateField<K extends keyof IBuyer>(field: K, value: IBuyer[K]): boolean {
+    if (this.validateField(field, value as string)) {
+      this.buyer[field] = value;
+      console.log(`Поле ${field} успешно обновлено`);
+      return true;
+    } else {
+      console.log(`Некорректное значение для поля ${field}`);
+      return false;
+    }
+  }
 
+  // Альтернативный вариант: отдельные методы для каждого поля
+  public setEmail(email: string): boolean {
+    return this.updateField('email', email);
+  }
+
+  public setPhone(phone: string): boolean {
+    return this.updateField('phone', phone);
+  }
+
+  public setAddress(address: string): boolean {
+    return this.updateField('address', address);
+  }
+
+  public setPayment(payment: 'card' | 'cash'): boolean {
+    return this.updateField('payment', payment);
+  }
+
+  // Полная проверка всех данных (для финальной валидации перед отправкой)
+  public isValid(): boolean {
     return (
-      !!buyer.payment && (buyer.payment === 'card' || buyer.payment === 'cash') &&
-      !!buyer.address && emailRegex.test(buyer.email) && phoneRegex.test(buyer.phone)
+      !!this.buyer.payment && 
+      !!this.buyer.address && 
+      this.validateField('email', this.buyer.email) && 
+      this.validateField('phone', this.buyer.phone)
     );
   }
 
-  public getData(): IBuyer {
-    return this.buyer;
-  }
+  public getData(): IBuyer { 
+    return { ...this.buyer }; // возвращаем копию для иммутабельности
+  } 
 
-  public clearData(): void {
-    this.buyer.payment = '';
-    this.buyer.address = '';
-    this.buyer.email = '';
-    this.buyer.phone = '';
-  }
+  public clearData(): void { 
+    this.buyer.payment = ''; 
+    this.buyer.address = ''; 
+    this.buyer.email = ''; 
+    this.buyer.phone = ''; 
+  } 
 
-  public setData(newBuyer: IBuyer): void {
-    if (this.checkData(newBuyer)) {
-      console.log('Данные покупателя валидны');
-      this.buyer.payment = newBuyer.payment;
-      this.buyer.address = newBuyer.address;
-      this.buyer.email = newBuyer.email;
-      this.buyer.phone = newBuyer.phone;
-    } else console.log('Данные покупателя некорректны');
-  }
+  // Старый метод оставлен для обратной совместимости, но можно deprecated
+  public setData(newBuyer: IBuyer): void { 
+    // Постепенное обновление с валидацией каждого поля
+    let allValid = true;
+    
+    if (newBuyer.payment) {
+      if (!this.updateField('payment', newBuyer.payment)) allValid = false;
+    }
+    
+    if (newBuyer.address) {
+      if (!this.updateField('address', newBuyer.address)) allValid = false;
+    }
+    
+    if (newBuyer.email) {
+      if (!this.updateField('email', newBuyer.email)) allValid = false;
+    }
+    
+    if (newBuyer.phone) {
+      if (!this.updateField('phone', newBuyer.phone)) allValid = false;
+    }
+
+    if (allValid) {
+      console.log('Все данные покупателя успешно обновлены и валидны');
+    } else {
+      console.log('Часть данных покупателя некорректна');
+    }
+  } 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,112 +1,121 @@
-import './types/index.ts'
-import './scss/styles.scss';
-import { Api } from "./components/base/Api";
-import { ComApi } from "./components/base/comApi/comApi.ts";
-import { Buyer } from "./components/Models/Buyer";
-import { Cart } from "./components/Models/Cart";
-import { Catalog } from "./components/Models/Catalog";
-import { API_URL } from "./utils/constants";
-import { IBuyer, IpostProducts, IProduct } from './types/index.ts';
+import './types/index.ts' 
+import './scss/styles.scss'; 
+import { Api } from "./components/base/Api"; 
+import { ComApi } from "./components/base/comApi/comApi.ts"; 
+import { Buyer } from "./components/Models/Buyer"; 
+import { Cart } from "./components/Models/Cart"; 
+import { Catalog } from "./components/Models/Catalog"; 
+import { API_URL } from "./utils/constants"; 
+import { IBuyer, IpostProducts, IProduct } from './types/index.ts'; 
 
-const comApi = new ComApi(new Api(API_URL));
+const comApi = new ComApi(new Api(API_URL)); 
 
-const catalog = new Catalog();
-const cart = new Cart();
-const buyer = new Buyer();
+const catalog = new Catalog(); 
+const cart = new Cart(); 
+const buyer = new Buyer(); 
 
-const product1: IProduct = {
-  "description": "Лизните этот леденец, чтобы мгновенно запоминать и узнавать любой цветовой код CSS.",
-  "image": "/Shell.svg",
-  "title": "HEX-леденец",
-  "category": "другое",
-  "price": 1450,
-  "id": "c101ab44-ed99-4a54-990d-47aa2bb4e7d9",
-}
+const product1: IProduct = { 
+  "description": "Лизните этот леденец, чтобы мгновенно запоминать и узнавать любой цветовой код CSS.", 
+  "image": "/Shell.svg", 
+  "title": "HEX-леденец", 
+  "category": "другое", 
+  "price": 1450, 
+  "id": "c101ab44-ed99-4a54-990d-47aa2bb4e7d9", 
+} 
 
-const product2: IProduct = {
-  "id": "b06cde61-912f-4663-9751-09956c0eed67",
-  "description": "Будет стоять над душой и не давать прокрастинировать.",
-  "image": "/Asterisk_2.svg",
-  "title": "Мамка-таймер",
-  "category": "софт-скил",
-  "price": null
-}
+const product2: IProduct = { 
+  "id": "b06cde61-912f-4663-9751-09956c0eed67", 
+  "description": "Будет стоять над душой и не давать прокрастинировать.", 
+  "image": "/Asterisk_2.svg", 
+  "title": "Мамка-таймер", 
+  "category": "софт-скил", 
+  "price": null 
+} 
 
-const buyer1: IBuyer = {
-  payment: 'card',
-  address: 'moscow city',
-  email: 'sanekm1901@mail.ru',
-  phone: '89209871358'
-}
+const buyer1: IBuyer = { 
+  payment: 'card', 
+  address: 'moscow city', 
+  email: 'sanekm1901@mail.ru', 
+  phone: '89209871358' 
+} 
 
-const buyer2: IBuyer = {
-  payment: 'cash',
-  address: 'Ryazan',
-  email: 'sanekm1901mailru',
-  phone: '898'
-}
+const buyer2: IBuyer = { 
+  payment: 'cash', 
+  address: 'Ryazan', 
+  email: 'sanekm1901mailru', 
+  phone: '898' 
+} 
 
-const data: IpostProducts = {
-  "payment": "cash",
-  "email": "test@test.ru",
-  "phone": "+71234567890",
-  "address": "Spb Vosstania 1",
-  "total": 2200,
-  "items": [
-    "854cef69-976d-4c2a-a18c-2aa45046c390",
-    "c101ab44-ed99-4a54-990d-47aa2bb4e7d9"
-  ]
-}
+const data: IpostProducts = { 
+  "payment": "cash", 
+  "email": "test@test.ru", 
+  "phone": "+71234567890", 
+  "address": "Spb Vosstania 1", 
+  "total": 2200, 
+  "items": [ 
+    "854cef69-976d-4c2a-a18c-2aa45046c390", 
+    "c101ab44-ed99-4a54-990d-47aa2bb4e7d9" 
+  ] 
+} 
 
-//Проверка методов корзины
+// Проверка методов корзины
+console.log('Методы корзины'); 
+console.log('Вывод данных коллекции в начальном состоянии', cart.getCartItems()); 
+console.log('Добавление продукта Product1 и Product2 в корзину'); 
+cart.addProduct(product1); 
+cart.addProduct(product2); 
+console.log(cart.getCartItems()); 
+console.log('Проверка наличия продукта Product1', cart.cartHasProduct(product1)); 
+console.log('Получение количества товаров в корзине', cart.getQuantityCartItems()); 
+console.log('Получение общей суммы товаров в корзине', cart.getTotalPrice()); 
+console.log('Удаление продукта Product1 из корзины'); 
+cart.deleteProduct(product1); 
+console.log(cart.getCartItems()); 
+console.log('Очистка корзины'); 
+cart.clearCart(); 
+console.log(cart.getCartItems()); 
 
-console.log('Методы корзины');
-console.log('Вывод данных коллекции в начальном состоянии', cart.getCartItems());
-console.log('Добавление продукта Product1 и Product2 в корзину');
-cart.addProduct(product1);
-cart.addProduct(product2);
-console.log(cart.getCartItems());
-console.log('Проверка наличия продукта Product1', cart.cartHasProduct(product1));
-console.log('Получение количества товаров в корзине', cart.getQuantityCartItems());
-console.log('Получение общей суммы товаров в корзине', cart.getTotalPrice());
-console.log('Удаление продукта Product1 из корзины');
-cart.deleteProduct(product1);
-console.log(cart.getCartItems());
-console.log('Очистка корзины');
-cart.clearCart();
-console.log(cart.getCartItems());
+console.log('______________________________________________________________________________________'); 
 
-console.log('______________________________________________________________________________________');
+// Проверка методов покупателя
+console.log('Методы покупателя'); 
+console.log('Вывод данных хранилищ в начальном состоянии', buyer.getData()); 
+console.log('Сохранение и проверка данных о покупателе'); 
+buyer.setData(buyer1); 
+console.log(buyer.getData()); 
+console.log('Очистка данных покупателя'); 
+buyer.clearData(); 
+console.log(buyer.getData()); 
+console.log('Сохранение и проверка данных о покупателе с невалидными даными'); 
+buyer.setData(buyer2); 
+console.log(buyer.getData()); 
 
-//Проверка методов покупателя
+console.log('______________________________________________________________________________________'); 
 
-console.log('Методы покупателя');
-console.log('Вывод данных хранилищ в начальном состоянии', buyer.getData());
-console.log('Сохранение и проверка данных о покупателе');
-buyer.setData(buyer1);
-console.log(buyer.getData());
-console.log('Очистка данных покупателя');
-buyer.clearData();
-console.log(buyer.getData());
-console.log('Сохранение и проверка данных о покупателе с невалидными даными');
-buyer.setData(buyer2);
-console.log(buyer.getData());
+// Проверка методов каталога и API
+console.log('Методы каталога'); 
+console.log('Вывод данных хранилищ в начальном состоянии', catalog.getProducts(), catalog.getPickedProduct()); 
+console.log('Заполнение каталога данными с сервера'); 
 
-console.log('______________________________________________________________________________________');
+// Загрузка данных каталога с обработкой ошибок
+comApi.get('/product/')
+  .then(res => {
+    catalog.setProducts(res);
+    console.log('Вывод заполненного массива товаров', catalog.getProducts()); 
+    console.log('Вывод товара по идентификатору из массива товаров', catalog.getProductByID("854cef69-976d-4c2a-a18c-2aa45046c390")); 
+    console.log('Сохранение товара Product1 в выбранный товар'); 
+    catalog.setPickedProduct(product1); 
+    console.log('Вывод данных из объекта выбранного товара', catalog.getPickedProduct()); 
+  })
+  .catch(error => {
+    console.error('Ошибка при загрузке каталога:', error);
+  });
 
-//Проверка методов каталога и api get
-
-console.log('Методы каталога');
-console.log('Вывод данных хранилищ в начальном состоянии', catalog.getProducts(), catalog.getPickedProduct());
-console.log('Заполнение каталога данными с сервера');
-comApi.get('/product/').then(res => catalog.setProducts(res));
-setTimeout(() => {
-console.log('Вывод заполненного массива товаров', catalog.getProducts());
-console.log('Вывод товара по идентификатору из массива товаров', catalog.getProductByID("854cef69-976d-4c2a-a18c-2aa45046c390"));
-console.log('Сохранение товара Product1 в выбранный товар');
-catalog.setPickedProduct(product1);
-console.log('Вывод данных из объекта выбранного товара', catalog.getPickedProduct());
-}, 1000);
-
-//Проверка метода api post
-comApi.post('/order/', data).then(resp => console.log(resp));
+// Проверка метода API post с обработкой ошибок
+comApi.post('/order/', data)
+  .then(resp => {
+    console.log('Успешный ответ от сервера:', resp);
+  })
+  .catch(error => {
+    console.error('Ошибка при отправке заказа:', error);
+  });
